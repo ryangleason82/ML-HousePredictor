d9c1598b4a6d3704c9977e79b689bfeef8dd55e0
Ryan Gleason <rjg5404@psu.edu>
2019-04-17 07:03:45 -0400

    Tensorflow Library installed and working

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,1 +1,16 @@
-hi;
+require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs-node");
+const loadCSV = require("./load-csv");
+
+let { features, labels, testFeatures, testLabels } = loadCSV(
+	"kc_house_data.csv",
+	{
+		shuffle: true,
+		splitTest: 10,
+		dataColumns: ["lat", "long"],
+		labelColumns: ["price"]
+	}
+);
+
+console.log(testFeatures);
+console.log(testLabels);
